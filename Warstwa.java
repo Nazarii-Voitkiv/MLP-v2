7df7394cb731bf9ae0b60de0219936e65bded51d
Nazarii Voitkiv <vojtkivnazar2005@gmail.com>
2025-03-27 11:36:19 +0100

    Delete all

--- Warstwa.java
+++ Warstwa.java
@@ -1,7 +1,7 @@
+
 public class Warstwa {
 	Neuron [] neurony;
 	int liczba_neuronow;
-	private double[] ostatnieWejscia; // останні входи шару
 	
 	public Warstwa(){
 		neurony=null;
@@ -14,46 +14,9 @@ public class Warstwa {
 			neurony[i]=new Neuron(liczba_wejsc);
 	}
 	double [] oblicz_wyjscie(double [] wejscia){
-		// Зберігаємо останні входи
-		ostatnieWejscia = new double[wejscia.length];
-		System.arraycopy(wejscia, 0, ostatnieWejscia, 0, wejscia.length);
-		
 		double [] wyjscie=new double[liczba_neuronow];
 		for(int i=0;i<liczba_neuronow;i++)
 			wyjscie[i]=neurony[i].oblicz_wyjscie(wejscia);
 		return wyjscie;
-	}
-	
-	// Обчислення дельт для вихідного шару - виправлена версія
-	public void obliczDeltyWyjsciowe(double[] oczekiwane) {
-		for (int i = 0; i < liczba_neuronow; i++) {
-			double wyjscie = neurony[i].getOstatnieWyjscie();
-			 // Використовуємо більш чіткий алгоритм
-			double delta = (oczekiwane[i] - wyjscie) * wyjscie * (1.0 - wyjscie);
-			neurony[i].setDelta(delta);
-		}
-	}
-	
-	// Обчислення дельт для прихованих шарів
-	public void obliczDeltyUkryte(Warstwa nastepnaWarstwa) {
-		for (int i = 0; i < liczba_neuronow; i++) {
-			double suma = 0.0;
-			// Сума зважених дельт з наступного шару
-			for (int j = 0; j < nastepnaWarstwa.liczba_neuronow; j++) {
-				suma += nastepnaWarstwa.neurony[j].getDelta() * nastepnaWarstwa.neurony[j].wagi[i+1];
-			}
-			
-			double wyjscie = neurony[i].getOstatnieWyjscie();
-			// Формула для прихованого шару: δ = y * (1 - y) * Σ(δ_j * w_ji)
-			double delta = wyjscie * (1.0 - wyjscie) * suma;
-			neurony[i].setDelta(delta);
-		}
-	}
-	
-	// Оновлення ваг нейронів у шарі
-	public void aktualizujWagi(double wspolczynnikNauki) {
-		for (int i = 0; i < liczba_neuronow; i++) {
-			neurony[i].aktualizujWagi(wspolczynnikNauki);
-		}
 	}
 }
