1a411854d8a53294b3eba40ca22da6cdc9435ec0
Nazarii Voitkiv <vojtkivnazar2005@gmail.com>
2025-03-27 23:25:38 +0100

    step 1

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public double[] input;
+    public double[] target;
+
+    public Sample(double[] input, double[] target) {
+        this.input = input;
+        this.target = target;
+    }
+}
